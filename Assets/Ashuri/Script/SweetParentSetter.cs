f98cbc31f4b6d6fe8e1d7ad1a66004526a07e2c5
TSITCL\it232004 <it232004@trident.ac.jp>
2025-11-12 18:55:29 +0900

    ステージの作成

--- Assets/Ashuri/Script/SweetParentSetter.cs
+++ Assets/Ashuri/Script/SweetParentSetter.cs
@@ -16,7 +16,7 @@ public class SweetParentSetter : NetworkBehaviour
         // ただし、NetworkServer.Spawn後、ClientRpcが呼ばれる前にクライアント側で親がルートになっている可能性もある
         // ここでは、isServer && isClient の場合はスキップせず、クライアント側でのみSetParentを実行する
         // if (isServer && isClient) return; // ホストの場合はスキップ (サーバー側で設定済みのため)
-
+            
         if (parentIdentity != null)
         {
             // クライアント側で親オブジェクトのTransformを取得し、自身の子に設定
@@ -25,7 +25,7 @@ public class SweetParentSetter : NetworkBehaviour
             // 親を設定した後、ローカル座標や回転をリセットして親に対する相対位置を調整する
             //transform.localPosition = Vector3.zero; // 親の原点に配置
             transform.localRotation = Quaternion.identity; // 親と同じ回転
-            transform.localScale = Vector3.one; // 親と同じスケール (必要に応じて)
+            //transform.localScale = Vector3.one; // 親と同じスケール (必要に応じて)
         }
         else
         {
