751461c47cd2f07282fe41d711294887a9f40611
YPT25 <143860858+YPT25@users.noreply.github.com>
2025-12-02 11:45:51 +0900

    タイトルシーンのロゴ演出のスクリプトの追加(Tanabe/Check/Title/)

---
+++ Assets/Tanabe/Check/Title/Check_TitleAnimation_Tanabe.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;  //DOTween‚ðŽg‚¤‚½‚ß‚ÉŒÄ‚Ño‚µ‚½
+
+public class Check_TitleAnimation_Tanabe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 
+        this.transform.DOMove(new Vector3(2.4f, 8.0f, -14.7f), 2.0f)
+            .SetEase(Ease.InOutBack)
+            .OnKill(() =>
+            {
+                this.Appeal();
+            }
+            );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Appeal()
+    {
+        float a = 2.6f;
+        float b = 2.3f;
+
+        this.transform.DOScale(new Vector3(a, a, a), 0.4f)
+            .SetEase(Ease.OutQuart)
+        .OnKill(() =>
+        {
+            this.transform.DOScale(new Vector3(b, b, b), 0.4f)
+            .SetEase(Ease.InQuart)
+            .OnKill(() =>
+            {
+                this.Appeal();
+            });
+        });
+    }
+}
