332baa8f8a5229e5d22658cd065d523ed66c0c63
YPT25 <143860858+YPT25@users.noreply.github.com>
2025-12-03 10:56:21 +0900

    シーン遷移等に使用できるローディング演出の作成。

--- Assets/Tanabe/Check/2DTest/PlayerTest2D_Tanabe.cs
+++ Assets/Tanabe/Check/2DTest/PlayerTest2D_Tanabe.cs
@@ -6,7 +6,8 @@ public class PlayerTest2D_Tanabe : MonoBehaviour
 {
     [SerializeField, Range(0f, 500f)] private float m_power;
     private Rigidbody2D m_rb;
-    private float m_timer = 1f;
+    [SerializeField] private float COUNTTIME = 2f;
+    [SerializeField] private float m_timer = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,7 @@ public class PlayerTest2D_Tanabe : MonoBehaviour
         m_timer -= Time.deltaTime;
         if(Input.GetKeyDown(KeyCode.Space) || m_timer <= 0f)
         {
-            m_timer = 2f;
+            m_timer = COUNTTIME;
             m_rb.AddForce(Vector3.up * m_power, ForceMode2D.Impulse);
             m_rb.angularVelocity = Random.Range(-15f, 15f);
         }
