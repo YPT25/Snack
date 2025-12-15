f301703aa82bb994a904e229920a4c8c9cd6f4c0
TSITCL\it232004 <it232004@trident.ac.jp>
2025-12-15 11:34:07 +0900

    3vs1とロビーの変更

--- Assets/Ashuri/Script/3VS1MODE/CanvasSwitcher_Ashuri.cs
+++ Assets/Ashuri/Script/3VS1MODE/CanvasSwitcher_Ashuri.cs
@@ -1,34 +1,60 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class CanvasSwitcher_Ashuri : MonoBehaviour
+public class CanvasSwitcher_Ashuri : NetworkBehaviour
 {
+    // チーム0用Canvas
+    [Header("Canvas設定")]
+    [Tooltip("チームIDが一人チームのときに表示する Canvas")]
+    [SerializeField] private GameObject teamFirstCanvas;
+
+    // チーム1用Canvas
+    [Tooltip("チームIDが三人チームのときに表示する Canvas")]
+    [SerializeField] private GameObject teamThirdCanvas;
+
     // ローカルプレイヤーの参照
     private Player_Tanabe _localPlayer;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        // チームを検索する
-        FindOfTeam();
+        // ローカルプレイヤーが生成されるまで待つ
+        if (NetworkClient.localPlayer == null)
+            return;
+
+        // ローカルプレイヤーの接続情報を取得
+        NetworkConnectionToClient conn =
+            NetworkClient.localPlayer.connectionToClient;
+
+        // StatePlayer を取得
+        StatePlayer_Ashuri state = StatePlayer_Ashuri.Instance;
+
+        // モードID（チームID）を取得
+        int teamId = state.GetModeId(conn);
+
+        // Canvas を切り替える
+        SwitchCanvas(teamId);
     }
 
-    // Update is called once per frame
-    void Update()
+    // ----------------------------------------------------
+    // Canvas 切り替え処理
+    // ----------------------------------------------------
+    private void SwitchCanvas(int teamId)
     {
-        
-    }
+        // 一度すべて非表示
+        teamFirstCanvas.SetActive(false);
+        teamThirdCanvas.SetActive(false);
 
-    // -----------------------------------------------
-    // 所持アイテムに合わせて UI を更新する処理
-    // -----------------------------------------------
-    private void FindOfTeam()
-    {
-        // StatePlayer_Ashuriを検索する
-        StatePlayer_Ashuri statePlayer_Ashuri = FindObjectOfType<StatePlayer_Ashuri>();
-
-        // チーム情報を取得する
+        // チームIDに応じて表示
+        if (teamId == 1)
+        {
+            teamFirstCanvas.SetActive(true);
+        }
+        else if (teamId == 2)
+        {
+            teamThirdCanvas.SetActive(true);
+        }
     }
 }
