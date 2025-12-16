62074c881162391a68fb05eb57dd95a41761982b
TSITCL\it232004 <it232004@trident.ac.jp>
2025-12-16 12:20:25 +0900

    デバックエラーの内容の訂正

--- Assets/Ashuri/Script/MaterialPlayerScript.cs
+++ Assets/Ashuri/Script/MaterialPlayerScript.cs
@@ -7,10 +7,10 @@ public class MaterialPlayerScript : NetworkBehaviour
     [SerializeField] private int _materialIndex;
 
     // 衝突した時に呼ばれる処理
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
         // 衝突相手から PlayerColorChanger を取得
-        var changer = collision.gameObject.GetComponentInParent<PlayerColorChanger>();
+        var changer = other.gameObject.GetComponentInParent<PlayerColorChanger>();
 
         // changer が存在し なおかつ ローカルプレイヤーなら実行
         if (changer != null && changer.isLocalPlayer)
